d0320fb0ab3938497de1f3df54693a5bf050072b
robmarkcole <robmarkcole@gmail.com>
2018-02-26 05:43:53 +0000

    Adds demo test

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,10 +1,10 @@
 """The tests for the simulated sensor."""
 import unittest
 
-from homeassistant.components.sensor.simulated import (
+from homeassistant.components.sensor.demo import (
     CONF_UNIT, CONF_AMP, CONF_MEAN, CONF_PERIOD, CONF_PHASE, CONF_FWHM,
-    CONF_SEED, DEFAULT_NAME, DEFAULT_AMP, DEFAULT_MEAN,
-    DEFAULT_PHASE, DEFAULT_FWHM, DEFAULT_SEED)
+    CONF_SEED, DEFAULT_NAME, DEFAULT_AMP, DEFAULT_MEAN, DEFAULT_UNIT,
+    DEFAULT_PHASE, DEFAULT_PERIOD, DEFAULT_FWHM, DEFAULT_SEED)
 from homeassistant.const import CONF_FRIENDLY_NAME
 from homeassistant.setup import setup_component
 from tests.common import get_test_home_assistant
@@ -25,23 +25,21 @@ class TestSimulatedSensor(unittest.TestCase):
         """Test default config."""
         config = {
             'sensor': {
-                'platform': 'simulated'}
+                'platform': 'demo'}
         }
         self.assertTrue(
             setup_component(self.hass, 'sensor', config))
         self.hass.block_till_done()
         assert len(self.hass.states.entity_ids()) == 1
-        state = self.hass.states.get('sensor.simulated')
+        state = self.hass.states.get('sensor.outside_humidity')
         assert state.attributes.get(
             CONF_FRIENDLY_NAME) == DEFAULT_NAME
         assert state.attributes.get(
             CONF_AMP) == DEFAULT_AMP
         assert state.attributes.get(
-            CONF_UNIT) is None
-        assert state.attributes.get(
             CONF_MEAN) == DEFAULT_MEAN
         assert state.attributes.get(
-            CONF_PERIOD) == 60.0
+            CONF_PERIOD) == DEFAULT_PERIOD
         assert state.attributes.get(
             CONF_PHASE) == DEFAULT_PHASE
         assert state.attributes.get(
